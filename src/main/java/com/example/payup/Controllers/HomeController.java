436b847a4b9a4e2454aaab51bd22afb5065d3987
Chris White <jcwhite99@gmail.com>
2018-08-05 12:40:38 -0500

    working home page

--- src/main/java/com/example/payup/Controllers/HomeController.java
+++ src/main/java/com/example/payup/Controllers/HomeController.java
@@ -1,14 +1,18 @@
 package com.example.payup.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("pay-up")
 public class HomeController {
 
-    @RequestMapping(value="/", method=RequestMethod.GET)
-        public String home() {
-            return "home";
+    @RequestMapping(value="", method=RequestMethod.GET)
+        public String index(Model model) {
+
+            model.addAttribute("title","Pay Up!" );
+            return "home/index";
         }
     }
