3a0adcbc058c7fd2ea335b38fcf6c8fedefd9134
Chris White <jcwhite99@gmail.com>
2018-08-11 18:00:53 -0500

    finished services form

--- src/main/java/com/example/payup/models/forms/Services.java
+++ src/main/java/com/example/payup/models/forms/Services.java
@@ -1,4 +1,56 @@
 package com.example.payup.models.forms;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Services {
+
+    @NotNull
+    @Size(min = 5, max = 15)
+    private String services;
+
+    @NotNull
+    @Size(min = 4, max = 8)
+    private int date;
+
+    @NotNull
+    @Size(min = 1, max = 6)
+    private int price;
+
+    public Services(String services, int date, int price){
+        this.services = services;
+        this.date = date;
+        this.price = price;
+
+    }
+
+    public Services() {}
+
+    public String getServices() {
+        return services;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
