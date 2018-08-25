56e9abc1b353915bc61f3f176e4faa64563508c6
Chris White <jcwhite99@gmail.com>
2018-08-24 20:58:46 -0500

    companies added to dashboard

--- src/main/java/com/example/payup/Controllers/DashboardController.java
+++ src/main/java/com/example/payup/Controllers/DashboardController.java
@@ -1,5 +1,9 @@
 package com.example.payup.Controllers;
 
+import com.example.payup.models.Company;
+import com.example.payup.models.data.CompanyDao;
+import com.example.payup.models.data.ServicesDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +13,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/pay-up/dashboard")
 public class DashboardController {
 
+    @Autowired
+    private CompanyDao companydao;
+
+    @Autowired
+    private ServicesDao servicesdao;
+
 
     @RequestMapping(value = "")
-    public String index(Model model) {
-        //model.addAttribute("company", companyDao.findAll());
+    public String index (Model model) {
         model.addAttribute("title", "Dashboard");
+        model.addAttribute("companies", companydao.findAll());
+        model.addAttribute("services", servicesdao.findAll());
         return "dashboard/main";
     }
+
 
 }
 
