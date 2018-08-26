0e435753763c674e47df99478904032d85ce4aed
Chris White <jcwhite99@gmail.com>
2018-08-26 16:23:14 -0500

    added javascript for invoice generation

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,22 @@
+function FillSelection(f){
+    if(f.selectiontoo.checked == true){
+    f.selectioncompany.value = f.invoicecompany.value;
+    f.selectionservices.value = f.invoiceservices.value;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
