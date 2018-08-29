ae1fd90f696904a997b472a3feb8a119fea8d780
Chris White <jcwhite99@gmail.com>
2018-08-29 15:15:38 -0500

    javascript to populate invoice added

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,5 +1,10 @@
+if (getField("Box1").value=="Yes")
+this.getField("invoice").value = this.getField("Companyk").value;
+this.getField("invoice").value = this.getField("field1").value;
 
-
+if (getField("Box1").value=="Off")
+this.getField("invoice").value = " ";
+this.getField("field2").value = " ";
 
 
 
