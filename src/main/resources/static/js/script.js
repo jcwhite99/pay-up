215f655e5817562bd02bcffc0d2a597f576b42c8
Chris White <jcwhite99@gmail.com>
2018-09-05 21:05:58 -0500

    added total

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -11,3 +11,16 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+
+function calculate(){
+    var field1=document.getElementById("$1").value;
+    var field2=document.getElementById("$2").value;
+
+
+    var result=parseFloat(field1)+parseFloat(field2);
+
+    if (!isNaN(result)){
+    }
+    document.getElementById("total").innerHTML="Please pay this amount"+result;
+    }
