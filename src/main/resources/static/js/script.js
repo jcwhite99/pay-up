570a9836322854e98ef889b8f3cafda312920720
Chris White <jcwhite99@gmail.com>
2018-09-06 16:59:55 -0500

    delete row broke project delete row deleted project working again

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -12,7 +12,6 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 
-
 function calculate(){
     var val1=parseInt(document.getElementById("value1").value);
     var val2=parseInt(document.getElementById("value2").value);
@@ -20,24 +19,4 @@ function calculate(){
     var ansD=document.getElementById('answer');
 
     ansD.value = val1 + val2 + val3;
-
 }
-
-
-function myFunction() {
-    var table = document.getElementById("invoice");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-
-}
-
-
-function deleteRow(r) {
-    var i = r.parentNode.parentNode.rowIndex;
-    document.getElementById("invoice").deleteRow(i);
-}
-
-
-
